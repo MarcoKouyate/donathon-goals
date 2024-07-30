4c2d85817989827e5c691c0cfbf7d2e7e18fb433
Marco Kouyaté <marco.kouyate@yahoo.fr>
2024-07-31 00:12:30 +0200

    Completed basic features + styling

--- src/app/components/SubGoal.tsx
+++ src/app/components/SubGoal.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import Image from "next/image";
+import iconActive from "./goal-icon_active.png";
+import iconInactive from "./goal-icon_inactive.png";
+import localFont from "next/font/local";
 
 interface SubGoalProps {
   title: string;
@@ -6,10 +9,36 @@ interface SubGoalProps {
   done: boolean;
 }
 
+const myFont = localFont({ src: "./Spaced OTF.otf" });
+
 const SubGoal = (props: SubGoalProps) => {
+  const common =
+    "text-md relative bg-gradient-to-r  rounded-full m-4 py-2 px-5 flex justify-between ";
+  const active =
+    "from-pink-300 to-teal-200 shadow-[rgba(255,255,255,0.5)_0px_0px_3px_3px]";
+  const inactive =
+    "from-cyan-700 to-slate-700 shadow-[rgba(125,125,255,0.2)_0px_0px_3px_3px]";
+
   return (
-    <div className="bg-gradient-to-r from-pink-300 to-teal-200 rounded-full m-2 py-2 px-5">
-      {props.title}
+    <div className={common + " " + (props.done ? active : inactive)}>
+      <Image
+        className="absolute -top-2 -left-6"
+        src={props.done ? iconActive : iconInactive}
+        alt="icon for successful goals"
+        width={55}
+        height={55}
+      />
+      <p className="pl-5 italic">{props.title}</p>
+      <p
+        className={
+          "px-2 py-0 text-white rounded-full w-15 text-right " +
+          (props.done
+            ? " bg-amber-200 text-gray-600"
+            : " bg-slate-500 shadow-[rgba(255,255,255,0.3)_0px_0px_2px_2px]")
+        }
+      >
+        {props.amount}
+      </p>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import SubGoal from "./components/SubGoal";
+import React, { useState } from "react";
 import goals from "./goals.json";
 
 export default function Home() {
+  const [indexCompleted, SetIndexCompleted] = useState(0);
+
   return (
     <main>
-      {goals.map((goal) => {
+      {goals.map((goal, index) => {
         return (
-          <SubGoal title={goal.title} done={goal.done} amount={goal.amount} />
+          <div
+            onClick={() => {
+              SetIndexCompleted(index);
+            }}
+          >
+            <SubGoal
+              title={goal.title}
+              done={index <= indexCompleted}
+              amount={goal.amount}
+            />
+          </div>
         );
       })}
     </main>
